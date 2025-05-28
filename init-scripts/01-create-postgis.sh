6bb5d449da2493355513ca17b6c6cbcc01041a76
Andreas Jensen <andreas@dhis2.org>
2025-05-28 12:27:45 +0700

    chore: Revert "fix(docker): resolve PostGIS initialization"
    This reverts commit 3d30b386cc55b7df89b7b2dadb3e56b00d3c40a0.

--- init-scripts/01-create-postgis.sh
+++ init-scripts/01-create-postgis.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-# Create PostGIS extension in the dhis database using postgres superuser
-PGPASSWORD="$POSTGRESQL_POSTGRES_PASSWORD" psql -v -U postgres -d "$POSTGRESQL_DATABASE" -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+psql -v -U $POSTGRESQL_USERNAME -c "CREATE EXTENSION postgis;"
