68903cb6ca2bd48a0ff7cedaa24c47218c6bd25a
Rado <radoslav@dhis2.org>
2025-09-11 17:20:29 +0300

    refactor: swap bitnami/postgres for postgis/postgis
    Signed-off-by: Rado <radoslav@dhis2.org>

--- init-scripts/01-create-postgis.sh
+++ init-scripts/01-create-postgis.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
+set -e
 
-psql -v -U $POSTGRESQL_USERNAME -c "CREATE EXTENSION postgis;"
+psql -v -U $POSTGRES_USER -d $POSTGRES_DB -c "CREATE EXTENSION IF NOT EXISTS postgis;"
+
+psql -v -U $POSTGRES_USER -d $POSTGRES_DB -c "SELECT PostGIS_Version();"
