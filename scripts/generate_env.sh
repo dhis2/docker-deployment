5a90c60ddad8931ae2dee0b65130a4db54693445
tonsV2 <tonsV2@users.noreply.github.com>
2025-09-20 08:31:02 +0700

    feat: create dedicated DHIS2 user for monitoring (#19)
    * feat: create dedicated DHIS2 user for monitoring
    
    * chore: read Prometheus scraper user password from a secret
    
    * chore: update comment
    
    * chore: plain values
    
    * chore: install Loki logging driver

--- scripts/generate_env.sh
+++ scripts/generate_env.sh
@@ -5,6 +5,17 @@ CHARSET='A-Za-z0-9_=.-'
 OUTPUT_FILE=".env"
 TEMPLATE_FILE=".env.example"
 
+generate_password() {
+    local password=""
+    password+=$(LC_ALL=C tr -dc 'A-Z' < /dev/urandom | head -c 1)
+    password+=$(LC_ALL=C tr -dc 'a-z' < /dev/urandom | head -c 1)
+    password+=$(LC_ALL=C tr -dc '0-9' < /dev/urandom | head -c 1)
+    password+=$(LC_ALL=C tr -dc '_=.-' < /dev/urandom | head -c 1)
+    local remaining=$((LENGTH - 4))
+    password+=$(LC_ALL=C tr -dc "$CHARSET" < /dev/urandom | head -c "$remaining")
+    echo "$password" | fold -w1 | shuf | tr -d '\n'
+}
+
 if [ -f "$OUTPUT_FILE" ]; then
   echo "Error: An '$OUTPUT_FILE' file already exists." >&2
   exit 1
@@ -15,24 +26,24 @@ if [ ! -f "$TEMPLATE_FILE" ]; then
   exit 1
 fi
 
-POSTGRES_PASSWORD=$(LC_ALL=C tr -dc "$CHARSET" < /dev/urandom | head -c "$LENGTH")
-POSTGRES_DB_PASSWORD=$(LC_ALL=C tr -dc "$CHARSET" < /dev/urandom | head -c "$LENGTH")
-GRAFANA_ADMIN_PASSWORD=$(LC_ALL=C tr -dc "$CHARSET" < /dev/urandom | head -c "$LENGTH")
+POSTGRES_PASSWORD=$(generate_password)
+POSTGRES_DB_PASSWORD=$(generate_password)
+GRAFANA_ADMIN_PASSWORD=$(generate_password)
+DHIS2_MONITOR_PASSWORD=$(generate_password)
 
 cp "$TEMPLATE_FILE" "$OUTPUT_FILE"
 
 # Detect GNU vs BSD sed
 if sed --version >/dev/null 2>&1; then
-  # GNU sed
-  sed -i "s/^POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$POSTGRES_PASSWORD/" "$OUTPUT_FILE"
-  sed -i "s/^POSTGRES_DB_PASSWORD=.*/POSTGRES_DB_PASSWORD=$POSTGRES_DB_PASSWORD/" "$OUTPUT_FILE"
-  sed -i "s/^GRAFANA_ADMIN_PASSWORD=.*/GRAFANA_ADMIN_PASSWORD=$GRAFANA_ADMIN_PASSWORD/" "$OUTPUT_FILE"
+  SED_FLAGS="-i"
 else
-  # BSD sed (macOS)
-  sed -i '' "s/^POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$POSTGRES_PASSWORD/" "$OUTPUT_FILE"
-  sed -i '' "s/^POSTGRES_DB_PASSWORD=.*/POSTGRES_DB_PASSWORD=$POSTGRES_DB_PASSWORD/" "$OUTPUT_FILE"
-  sed -i '' "s/^GRAFANA_ADMIN_PASSWORD=.*/GRAFANA_ADMIN_PASSWORD=$GRAFANA_ADMIN_PASSWORD/" "$OUTPUT_FILE"
+  SED_FLAGS="-i ''"
 fi
+
+sed "$SED_FLAGS" "s/^POSTGRES_PASSWORD=.*/POSTGRES_PASSWORD=$POSTGRES_PASSWORD/" "$OUTPUT_FILE"
+sed "$SED_FLAGS" "s/^POSTGRES_DB_PASSWORD=.*/POSTGRES_DB_PASSWORD=$POSTGRES_DB_PASSWORD/" "$OUTPUT_FILE"
+sed "$SED_FLAGS" "s/^GRAFANA_ADMIN_PASSWORD=.*/GRAFANA_ADMIN_PASSWORD=$GRAFANA_ADMIN_PASSWORD/" "$OUTPUT_FILE"
+sed "$SED_FLAGS" "s/^DHIS2_MONITOR_PASSWORD=.*/DHIS2_MONITOR_PASSWORD=$DHIS2_MONITOR_PASSWORD/" "$OUTPUT_FILE"
 
 chmod u+rw,go-rwx "$OUTPUT_FILE"
 
