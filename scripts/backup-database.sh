01952f7dfddb7fdc1e75a167da66d09deccdff5b
tonsV2 <tonsV2@users.noreply.github.com>
2025-10-01 22:27:42 +0700

    chore: timed and more robust scripts (#49)

--- scripts/backup-database.sh
+++ scripts/backup-database.sh
@@ -1,9 +1,10 @@
-#!/bin/sh
+#!/usr/bin/env bash
 
 set -o errexit
+set -o pipefail
 
 if [ "$POSTGRES_BACKUP_FORMAT" = "plain" ]; then
-  pg_dump --host "$POSTGRES_HOST" --username "$POSTGRES_USER" --format plain --exclude-table "analytics_*" --exclude-table "_*" "$POSTGRES_DB" --verbose | gzip > "/backups/$BACKUP_TIMESTAMP.sql.gz"
+  time (pg_dump --host "$POSTGRES_HOST" --username "$POSTGRES_USER" --format plain --exclude-table "analytics_*" --exclude-table "_*" "$POSTGRES_DB" --verbose | gzip > "/backups/$BACKUP_TIMESTAMP.sql.gz")
 else
-  pg_dump --host "$POSTGRES_HOST" --username "$POSTGRES_USER" --format "$POSTGRES_BACKUP_FORMAT" --exclude-table "analytics_*" --exclude-table "_*" --file "/backups/$BACKUP_TIMESTAMP.pgc" "$POSTGRES_DB" --verbose
+  time pg_dump --host "$POSTGRES_HOST" --username "$POSTGRES_USER" --format "$POSTGRES_BACKUP_FORMAT" --exclude-table "analytics_*" --exclude-table "_*" --file "/backups/$BACKUP_TIMESTAMP.pgc" "$POSTGRES_DB" --verbose
 fi
